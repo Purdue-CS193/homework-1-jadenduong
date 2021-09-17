b71f936847bd1bb31ffaad9629dfbaaee71cdd48
Jaden Ken Duong <jkduong@data.cs.purdue.edu>
2021-09-17 15:56:33 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,44 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ~/Homework/MA165/HW2.txt ~/HW1.txt
 
 # Task 2
-
+rm ~/Memes/Spongebob/money.jpeg
 
 # Task 3
-
+mv ~/Memes/Star Wars/fafsa.jpg ~/Memes/Spongebob
 
 # Task 4
-
+rm ~/Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv ~/Programs/Age.cpp ~/Programs/C++
 
 # Task 7
-
+cp ~/Programs/Java ~/Homework/CS180/Project_1
 
 # Task 8
-
+mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Prgrams/C
 
 # Task 9
-
+mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/Python 3
 
 # Task 10
-
+touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+ls -l 
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
+:set mouse=a
+:set number
+:set clipboard=unnamed,unnamedplus
 
 
 '
